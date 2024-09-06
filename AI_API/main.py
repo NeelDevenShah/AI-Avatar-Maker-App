2fe8d02666fb47a8c30f8304914c30bd90a550ec
Neel Shah <neeldevenshah@gmail.com>
2024-09-06 23:16:17 +0530

    Added the nest_asyncio for resolving the error

--- AI_API/main.py
+++ AI_API/main.py
@@ -14,6 +14,7 @@ import zipfile
 from PIL import Image
 from diffusers import StableDiffusionXLPipeline, EulerAncestralDiscreteScheduler
 from pyngrok import ngrok
+import nest_asyncio
 
 app = FastAPI()
 
